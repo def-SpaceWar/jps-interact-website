31c1557db8010a1ee13606bd418b565527d3a75f
Taptaplit <74630038+Taptaplit@users.noreply.github.com>
2023-07-24 16:58:35 -0400

    added photos

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,10 +1,35 @@
+<script lang="ts">
+	let photos: BigInteger = 25;
+	let photosPerColumn: BigInteger = parseInt(photos / 3);
+	let photos_1: BigInteger = photosPerColumn;
+	let photos_2: BigInteger = photos_1 + photosPerColumn;
+	let photos_3: BigInteger = photos_2 + photosPerColumn;
+</script>
+
 <svelte:head>
 	<title>Photos - JPS Interact Club</title>
 	<meta name="description" content="Learn more about the JPS Interact Club" />
 </svelte:head>
-<h1 class="title">PHOTOS</h1>
+<h1 class="title">Photos</h1>
 <hr />
 <div class="container">
+	<div class="photo-container">
+		<div class="column">
+			{#each Array.from(Array(photos_1 + 1).keys()).slice(1) as i}
+				<img src={'/photos/' + i + '.png'} alt="Photo of Club" class="photo" />
+			{/each}
+		</div>
+		<div class="column">
+			{#each Array.from(Array(photos_2 + 1).keys()).slice(photos_1 + 1) as i}
+				<img src={'/photos/' + i + '.png'} alt="Photo of Club" class="photo" />
+			{/each}
+		</div>
+		<div class="column">
+			{#each Array.from(Array(photos_3 + 1).keys()).slice(photos_2 + 1) as i}
+				<img src={'/photos/' + i + '.png'} alt="Photo of Club" class="photo" />
+			{/each}
+		</div>
+	</div>
 </div>
 
 <style>
@@ -15,7 +40,7 @@
 	}
 
 	.container {
-		padding: 0rem 20% 8rem 20%;
+		padding: 0rem 15% 9rem 15%;
 	}
 
 	.title {
@@ -24,8 +49,33 @@
 		font-weight: 500;
 		/* background-color: var(--primary);
 		color: var(--background); */
-		padding: 8rem 8rem 0rem 8rem;
+		padding: 8rem 0rem 0rem 0rem;
 		text-align: center;
 		font-family: 'Rubik', sans-serif;
 	}
+
+	.photo-container {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.column {
+		width: 30%;
+		margin: 0.2rem 0.2rem;
+		display: inline-block;
+		overflow: hidden;
+	}
+
+	.photo {
+		margin: 0;
+		margin: 0.1rem 0.1rem;
+		width: 100%;
+		object-fit: cover;
+		min-height: 300px;
+		max-height: 400px;
+		overflow: hidden;
+	}
+
+
+	
 </style>
